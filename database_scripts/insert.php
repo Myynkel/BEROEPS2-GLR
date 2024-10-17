5cb23f9aedace4aedbc477e546302f486592bae2
101342 <101342@glr.nl>
2024-10-17 10:40:10 +0200

    paar dingen aangepast - Pablo

--- database_scripts/insert.php
+++ database_scripts/insert.php
@@ -1,1 +1,9 @@
 <?php
+require '../config.php';
+
+$recepten_naam = 'Nieuw Recept';
+$ingredienten  = ['Zout','Peper','Olijfolie'];
+$sql_recept = 'INSERT into Recepten (ReceptNaam) VALUES(:naam)';
+
+
+
